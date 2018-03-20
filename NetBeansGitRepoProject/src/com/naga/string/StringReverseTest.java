d831dc1b6c44dd3743f298d4ce75df7272758b2b
NSN <ponugoti.nagaraj@gmail.com>
2018-03-20 05:01:41 -0700

    a new class is added which is corejava concepts

---
+++ NetBeansGitRepoProject/src/com/naga/string/StringReverseTest.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.naga.string;
+
+/**
+ *
+ * @author NSN
+ */
+public class StringReverseTest {
+    public static void main(String...args){
+        String str="nagaraj";
+       char[] a=str.toCharArray();
+       String str1=null;
+               char[] b=new char[a.length];
+       for(int i=a.length-1;i<=a.length-1;i--){
+         // b=a[i];
+       }
+    }
+    
+}
